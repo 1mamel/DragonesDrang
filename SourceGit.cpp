7fa6d8c9d2ac8081c025f9b5f30923ca4647772e
izikuu <crymee@o2.pl>
2014-05-25 18:34:14 +0200

    Lista itemów + dobre kordy w poziomach
    I kilka mini bugów hue

--- SourceGit.cpp
+++ SourceGit.cpp
@@ -129,9 +129,9 @@ public:
 	int crit;
 	int wartoscPrzedmiotu;
 	int obrona;
-	int izrecznosc;
-	int iinteligencja;
-	int isila;
+	int iZrecznosc;
+	int iInteligencja;
+	int iSila;
 	int tempexp;
 	int litera;
 	bool wykonanoRuch;
@@ -234,7 +234,7 @@ public:
 		pozioma = 205;
 	}
 
-	int zliczdef()
+	int zliczDef()
 	{
 		int defik=0;
 		for (int i=1;i<100;i++){
@@ -245,7 +245,7 @@ public:
 		return defik;
 	}
 
-	int zliczdmg()
+	int zliczDmg()
 	{
 		int defik=0;
 		for (int i=100;i<200;i++){
@@ -256,37 +256,37 @@ public:
 		return defik;
 	}
 
-	int zliczzrecznosc()
+	int zliczZrecznosc()
 	{
-		int dzrecznosc=0;
+		int dZrecznosc=0;
 		for (int i=1;i<200;i++){
 			items(i);
 			if (postac.posiadanePrzedmioty[i]==2)
-				dzrecznosc=dzrecznosc+izrecznosc; 
+				dZrecznosc=dZrecznosc+iZrecznosc; 
 		}
-		return dzrecznosc;
+		return dZrecznosc;
 	}
 
-	int zliczsile()
+	int zliczSile()
 	{
-		int dsila=0;
+		int dSila=0;
 		for (int i=1;i<200;i++){
 			items(i);
 			if (postac.posiadanePrzedmioty[i]==2)
-				dsila=dsila+isila; 
+				dSila=dSila+iSila; 
 		}
-		return dsila;
+		return dSila;
 	}
 
-	int zliczinteligencje()
+	int zliczInteligencje()
 	{
-		int dinteligencja=0;
+		int dInteligencja=0;
 		for (int i=1;i<200;i++){
 			items(i);
 			if (postac.posiadanePrzedmioty[i]==2)
-				dinteligencja=dinteligencja+iinteligencja; 
+				dInteligencja=dInteligencja+iInteligencja; 
 		}
-		return dinteligencja;
+		return dInteligencja;
 	}
 
 	void ramka(int ileDodatkowo = 0)
@@ -521,7 +521,7 @@ public:
 		ramkaWyboru("Co chcesz zrobic?","Wyrusz...|Karczma|Kowal i ekwipunek|Alchemik|Zobacz statystyki postaci|Zapisz stan gry|Opcje|Lista Posiadanych przedmiotow|");
 		if (wybor == 1)
 		{
-			ramkaWyboru("Gdzie chcialbys wyruszyc?", "Ayleid (latwy)|Dasek Moor (normalny)|Lochy cmentarza (normalny++)|Krypta Pacmana (chaos)|Sancre Tor (trudny)|Leze smoka (BOSS)|Powrot|");
+			ramkaWyboru("Gdzie chcialbys wyruszyc?", "Ayleid (latwy)|Dasek Moor (normalny)|Lochy cmentarza (normalny++)|Krypta Pacmana (chaos)|Sancre Tor (trudny)|Poziom6 (-)|Leze smoka (BOSS)|Powrot|");
 			if (wybor !=8)
 				level(wybor);
 			else 
@@ -731,7 +731,7 @@ public:
 	void atakNormalnyGracza()
 	{
 		odswiezEkranWalki();
-		dmg = int((rand() % 7)*0.1*postac.sila + postac.sila+zliczdmg()+zliczsile());
+		dmg = int((rand() % 7)*0.1*(postac.sila+zliczSile()) + postac.sila+zliczDmg()+zliczSile());
 		podejdzDoWroga();
 		if ( ((rand() % 99)+1)< crit)
 		{
@@ -758,7 +758,7 @@ public:
 		ktoryRuch = 0;
 		if (wybor == 1)//uleczenie
 		{
-			postac.hp= postac.hp + int((postac.inteligencja+zliczinteligencje())*1.5) ;
+			postac.hp= postac.hp + int((postac.inteligencja+zliczInteligencje())*1.5) ;
 			if (postac.hp> postac.maxhp) postac.hp =postac.maxhp;
 			postac.mp=postac.mp-5;
 			mciSendString("play sounds/heal.mp3 ",NULL,1,NULL);
@@ -768,7 +768,7 @@ public:
 		{
 			postac.mp-=5;
 			czyTrafienieKrytyczne = 0;
-			dmg = int((rand() % 7)*0.1*postac.inteligencja + (postac.inteligencja+zliczinteligencje())*2);
+			dmg = int((rand() % 7)*0.1*postac.inteligencja + (postac.inteligencja+zliczInteligencje())*2);
 			odswiezEkranWalki();
 			poslijPocisk(12,12);
 			if ( ((rand() % 99)+1)< crit)
@@ -815,7 +815,7 @@ public:
 		if (hpPotwora >0){
 			int pdmg;
 			odswiezEkranWalki();
-			pdmg = potdmgmin+(rand()%7) -zliczdef();
+			pdmg = potdmgmin+(rand()%7) -zliczDef();
 			if (pdmg<0) pdmg = 0;
 			if (stanDefensywny == true) pdmg = int(pdmg/2);
 			postac.hp=postac.hp-pdmg;
@@ -881,7 +881,7 @@ public:
 			else if (wybor == 3)
 			{
 				ktoryRuch = 3;
-				ramkaWyboru("Spellbook:", "Uzdrowienie za " + to_string(int((postac.inteligencja+zliczinteligencje())*1.5)) +string(" hp - 5 many|Ognisty Podmuch - 5 many|Przyspieszenie - 5 many|Powrot|"));
+				ramkaWyboru("Spellbook:", "Uzdrowienie za " + to_string(int((postac.inteligencja+zliczInteligencje())*1.5)) +string(" hp - 5 many|Ognisty Podmuch - 5 many|Przyspieszenie - 5 many|Powrot|"));
 				system("cls");
 				odswiezEkranWalki();
 				if (wybor == 1 || wybor == 2 || wybor == 3)
@@ -1046,7 +1046,7 @@ public:
 
 	void pokaz()
 	{
-		crit=int(sqrt(postac.zrecznosc)*4+zliczzrecznosc());
+		crit=int(sqrt(postac.zrecznosc+zliczZrecznosc())*4);
 		gotoxy(0,0);bialy();cout << doswiadczenie;szary();cout << " - ";bialy(); cout << postac.doswiadczenie<<"/"<<postac.maksymalneDoswiadczenie;szary();cout << "(";bialy();cout << postac.poziom;szary();cout<<")";
 		gotoxy(0,1);czerwony();cout << zycie;szary();cout << " - " ;czerwony();cout << postac.hp;szary();cout << "/";czerwony();cout <<postac.maxhp;
 		gotoxy(0,2);niebieski();cout << krzyz;szary();cout << " - " ;niebieski();cout << postac.mp;szary();cout<<"/";niebieski();cout<<postac.maxmp;
@@ -1057,16 +1057,16 @@ public:
 		gotoxy(0,3);
 		cout<< "obrona: ";
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
-		cout <<zliczdef() ;
+		cout <<zliczDef() ;
 		szary();
 		gotoxy(70,3);
 		cout << "Dmg: ";
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 240);
-		cout << postac.sila+zliczdmg()+zliczsile();
+		cout << postac.sila+zliczDmg()+zliczSile();
 		szary();
 		cout << "-";
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 240);
-		cout  << 6*0.1*postac.sila + postac.sila+zliczdmg()+zliczsile();
+		cout  << 6*0.1*postac.sila + postac.sila+zliczDmg()+zliczSile();
 		szary();
 		gotoxy(0,5);
 	}
@@ -1081,7 +1081,7 @@ public:
 			ramkaInformacji("GRATULACJE! Awansowales na poziom: " + to_string(postac.poziom+1),"hp: " + to_string(postac.maxhp) + string("->") + to_string(postac.maxhp+postac.budowa*1.2) + string("   mp: " )+ to_string(postac.maxmp) + string("->") + to_string(postac.maxmp*1.4));
 			postac.doswiadczenie = postac.doswiadczenie-postac.maksymalneDoswiadczenie;
 			postac.maksymalneDoswiadczenie=int(postac.maksymalneDoswiadczenie*1.4);
-			postac.maxhp=int(postac.maxhp+postac.budowa+*1.2);
+			postac.maxhp=int(postac.maxhp+postac.budowa*1.2);
 			postac.maxmp=int(postac.maxmp*1.4);
 			postac.mp=postac.maxmp;
 			postac.hp=postac.maxhp;
@@ -1237,7 +1237,7 @@ public:
 	void staty()
 	{
 		system("cls");
-		cout << "Poziom: " << postac.poziom <<"\nDoswiadczenie: "<< postac.doswiadczenie << "/" << postac.maksymalneDoswiadczenie<< "\nHP: " << postac.hp << "/" << postac.maxhp<< "\nMP: " << postac.mp << "/" << postac.maxmp<< "\n\nSila: "<<postac.sila<<" + "<<zliczsile()<<"\nZrecznosc: "<<postac.zrecznosc <<" + "<<zliczzrecznosc()<<"\nBudowa: " <<postac.budowa << "\nInteligencja: " << postac.inteligencja<<" + "<<zliczinteligencje()<<endl<<endl;
+		cout << "Poziom: " << postac.poziom <<"\nDoswiadczenie: "<< postac.doswiadczenie << "/" << postac.maksymalneDoswiadczenie<< "\nHP: " << postac.hp << "/" << postac.maxhp<< "\nMP: " << postac.mp << "/" << postac.maxmp<< "\n\nSila: "<<postac.sila<<" + "<<zliczSile()<<"\nZrecznosc: "<<postac.zrecznosc <<" + "<<zliczZrecznosc()<<"\nBudowa: " <<postac.budowa << "\nInteligencja: " << postac.inteligencja<<" + "<<zliczInteligencje()<<endl<<endl;
 		cout << "Szansa na krytyczny cios: " << crit << "%\n\n";
 		cout << "Sila wplywa na ilosc zadawanych obrazen\nZrecznosc wplywa na szanse krytycznego trafienia\nBudowa wplywa na ilosc maksymalnego hp po zdobyciu poziomu\nInteligencja wplywa na moc czarow\n\n";
 		system("PAUSE");    
@@ -1297,50 +1297,57 @@ public:
 
 	void items(int id) 
 	{
-		if(id==1){nazwaitemu = "Skorzany kaptur"; obrona = 1; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 120;}
-		if(id==2){nazwaitemu = "Kolczy czepiec"; obrona = 2; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 220;}
-		if(id==3){nazwaitemu = "Zelazny helm"; obrona = 4; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 420;}
-		if(id==4){nazwaitemu = "Stalowy helm"; obrona = 6; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 620;}
-		if(id==5){nazwaitemu = "Szklany helm"; obrona = 8; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 860;}
-		if(id==6){nazwaitemu = "Krasnoludzki szturmak"; obrona = 11; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 1200;}
-		if(id==20){nazwaitemu = "Skorzana zbroja"; obrona = 5; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 500;}
-		if(id==21){nazwaitemu = "Kolczuga"; obrona = 7; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 620;}
-		if(id==22){nazwaitemu = "Zelazna zbroja"; obrona = 9; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 840;}
-		if(id==23){nazwaitemu = "Stalowa zbroja"; obrona = 11; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 1200;}
-		if(id==24){nazwaitemu = "Szklana zbroja"; obrona = 13; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 1500;}
-		if(id==25){nazwaitemu = "Krasnoludzka zbroja"; obrona = 15; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 2000;}
-		if(id==40){nazwaitemu = "Skorzane buty"; obrona = 2; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 240;}
-		if(id==41){nazwaitemu = "Kolcze buty"; obrona = 3; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 370;}
-		if(id==42){nazwaitemu = "Zelazne buty"; obrona = 4; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 500;}
-		if(id==43){nazwaitemu = "Stalowe buty"; obrona = 6; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 715;}
-		if(id==44){nazwaitemu = "Szklane buty"; obrona = 8; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 900;}
-		if(id==45){nazwaitemu = "Krasnoludzkie buty"; obrona = 11; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 1200;}
-		if(id==60){nazwaitemu = "Skorzane spodnie"; obrona = 3; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 300;}
-		if(id==61){nazwaitemu = "Kolcze spodnie"; obrona = 5; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 450;}
-		if(id==62){nazwaitemu = "Zelazne nagolenniki"; obrona = 7; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 600;}
-		if(id==63){nazwaitemu = "Stalowe nagolenniki"; obrona = 9; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 700;}
-		if(id==64){nazwaitemu = "Szklane nagolenniki"; obrona = 11; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 700;}
-		if(id==65){nazwaitemu = "Krasnoludzkie nagolenniki"; obrona = 14; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 1600;}
-		if(id==80){nazwaitemu = "Skorzane rekawice"; obrona = 1; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 100;}
-		if(id==81){nazwaitemu = "Kolcze rekawice"; obrona = 2; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 200;}
-		if(id==82){nazwaitemu = "Zelazne rekawice"; obrona = 3; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 340;}
-		if(id==83){nazwaitemu = "Stalowe rekawice"; obrona = 4; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 550;}
-		if(id==84){nazwaitemu = "Szklane rekawice"; obrona = 6; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 800;}
-		if(id==85){nazwaitemu = "Krasnoludzkie rekawice"; obrona = 8; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 1100;}
-		if(id==100){nazwaitemu = "Zardzewialy zelazny sztylet"; obrona = 2; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 100;}
-		if(id==101){nazwaitemu = "Zelazny sztylet"; obrona = 5; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 400;}
-		if(id==102){nazwaitemu = "Stalowy sztylet"; obrona = 10; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 800;}
-		if(id==103){nazwaitemu = "Zelazny krotki miecz"; obrona = 15; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 1600;}
-		if(id==104){nazwaitemu = "Walerianski krotki miecz"; obrona = 20; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 2500;}
-		if(id==105){nazwaitemu = "Stalowy krotki miecz"; obrona = 25; izrecznosc=0; iinteligencja=0;   isila=0; wartoscPrzedmiotu = 3200;}
+		if(id==1){nazwaitemu = "Skorzany kaptur"; obrona = 1; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 120;}
+		if(id==2){nazwaitemu = "Kolczy czepiec"; obrona = 2; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 220;}
+		if(id==3){nazwaitemu = "Zelazny helm"; obrona = 4; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 420;}
+		if(id==4){nazwaitemu = "Stalowy helm"; obrona = 6; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 620;}
+		if(id==5){nazwaitemu = "Szklany helm"; obrona = 8; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 860;}
+		if(id==6){nazwaitemu = "Krasnoludzki szturmak"; obrona = 11; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 1200;}
+		if(id==20){nazwaitemu = "Skorzana zbroja"; obrona = 5; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 500;}
+		if(id==21){nazwaitemu = "Kolczuga"; obrona = 7; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 620;}
+		if(id==22){nazwaitemu = "Zelazna zbroja"; obrona = 9; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 840;}
+		if(id==23){nazwaitemu = "Stalowa zbroja"; obrona = 11; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 1200;}
+		if(id==24){nazwaitemu = "Szklana zbroja"; obrona = 13; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 1500;}
+		if(id==25){nazwaitemu = "Krasnoludzka zbroja"; obrona = 15; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 2000;}
+		if(id==40){nazwaitemu = "Skorzane buty"; obrona = 2; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 240;}
+		if(id==41){nazwaitemu = "Kolcze buty"; obrona = 3; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 370;}
+		if(id==42){nazwaitemu = "Zelazne buty"; obrona = 4; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 500;}
+		if(id==43){nazwaitemu = "Stalowe buty"; obrona = 6; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 715;}
+		if(id==44){nazwaitemu = "Szklane buty"; obrona = 8; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 900;}
+		if(id==45){nazwaitemu = "Krasnoludzkie buty"; obrona = 11; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 1200;}
+		if(id==60){nazwaitemu = "Skorzane spodnie"; obrona = 3; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 300;}
+		if(id==61){nazwaitemu = "Kolcze spodnie"; obrona = 5; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 450;}
+		if(id==62){nazwaitemu = "Zelazne nagolenniki"; obrona = 7; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 600;}
+		if(id==63){nazwaitemu = "Stalowe nagolenniki"; obrona = 9; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 700;}
+		if(id==64){nazwaitemu = "Szklane nagolenniki"; obrona = 11; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 700;}
+		if(id==65){nazwaitemu = "Krasnoludzkie nagolenniki"; obrona = 14; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 1600;}
+		if(id==80){nazwaitemu = "Skorzane rekawice"; obrona = 1; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 100;}
+		if(id==81){nazwaitemu = "Kolcze rekawice"; obrona = 2; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 200;}
+		if(id==82){nazwaitemu = "Zelazne rekawice"; obrona = 3; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 340;}
+		if(id==83){nazwaitemu = "Stalowe rekawice"; obrona = 4; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 550;}
+		if(id==84){nazwaitemu = "Szklane rekawice"; obrona = 6; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 800;}
+		if(id==85){nazwaitemu = "Krasnoludzkie rekawice"; obrona = 8; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 1100;}
+		if(id==100){nazwaitemu = "Zardzewialy zelazny sztylet"; obrona = 2; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 100;}
+		if(id==101){nazwaitemu = "Zelazny sztylet"; obrona = 5; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 400;}
+		if(id==102){nazwaitemu = "Stalowy sztylet"; obrona = 10; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 800;}
+		if(id==103){nazwaitemu = "Zelazny krotki miecz"; obrona = 15; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 1600;}
+		if(id==104){nazwaitemu = "Walerianski krotki miecz"; obrona = 20; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 2500;}
+		if(id==105){nazwaitemu = "Stalowy krotki miecz"; obrona = 25; iZrecznosc=0; iInteligencja=0; iSila=0; wartoscPrzedmiotu = 3200;}
 	}
 
 	void pokazitem(int czyPrzedmiotPosiadany)
 	{
-		cout << nazwaitemu << "|| "<<obrona<< " || Wartosc: " << wartoscPrzedmiotu;
+		cout << nazwaitemu << " || Wartosc: " << wartoscPrzedmiotu;
 		if (czyPrzedmiotPosiadany == 1) cout << " (Posiadasz)\n";
 		if (czyPrzedmiotPosiadany  == 2 ) cout <<" (Nosisz)\n";
 		if (czyPrzedmiotPosiadany == 0 ) cout << endl;
+		cout << "\tObrona: "<<obrona;
+		if(iSila>0)
+			cout << " || Sila: " <<iSila;
+		if(iZrecznosc>0)
+			cout << " || Zrecznosc: " <<iZrecznosc;
+		if(iInteligencja>0)
+			cout << " || Inteligencja: " <<iInteligencja <<endl;
 	}
 	string wyswietlItem(int idItemu)
 	{
@@ -1382,7 +1389,7 @@ public:
 	{     
 		items(ktoryPrzedmiot);
 		system("cls");
-		pokaz();
+		//pokaz(); z tym pokazuje przy nazwie itemu caly czas ostatni item. IDK po co tu to bylo :O
 		if (postac.posiadanePrzedmioty[ktoryPrzedmiot] ==1 || postac.posiadanePrzedmioty[ktoryPrzedmiot]==2)
 		{
 			ramkaWyboru("Co chcialbys zrobic z " + nazwaitemu + string("?"),"Sprzedac za " + to_string(int(wartoscPrzedmiotu/1.2)) + string(" sztuk zlota|Ubrac|Powrot...|"));
